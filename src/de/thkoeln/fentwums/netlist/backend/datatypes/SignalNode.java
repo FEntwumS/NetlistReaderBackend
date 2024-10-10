c06477dad8a9f38c3a1f84fabf4842bfd6bd4b10
Florian Kämpchen <florian.kaempchen@smail.th-koeln.de>
2024-10-10 09:41:24 +0200

    Add SignalNode.java

---
+++ src/de/thkoeln/fentwums/netlist/backend/datatypes/SignalNode.java
@@ -0,0 +1,106 @@
+package de.thkoeln.fentwums.netlist.backend.datatypes;
+
+import java.util.ArrayList;
+
+public class SignalNode {
+    private String sName;
+    private int sId;
+    private int lId;
+    private SignalNode hParent;
+    private ArrayList<SignalNode> hChildren;
+    private SignalNode sParent;
+    private ArrayList<SignalNode> sChildren;
+    private ArrayList<SignalNode> sLateralConnections;
+    private boolean sVisited;
+
+    public SignalNode() {
+        sName = "";
+        sId = 0;
+        hParent = null;
+        hChildren = new ArrayList<SignalNode>(8);
+        sParent = null;
+        sChildren = new ArrayList<SignalNode>(8);
+        sLateralConnections = new ArrayList<SignalNode>(4);
+        sVisited = false;
+    }
+
+    public SignalNode(String sName, SignalNode hParent, ArrayList<SignalNode> hChildren, SignalNode sParent, ArrayList<SignalNode> sChildren, ArrayList<SignalNode> sLateralConnections) {
+        this.sName = sName;
+        this.hParent = hParent;
+        this.hChildren = hChildren;
+        this.sParent = sParent;
+        this.sChildren = sChildren;
+        this.sLateralConnections = sLateralConnections;
+
+        if(hParent != null && hParent.getHChildren() != null) {
+            this.lId = hParent.getHChildren().size();
+            hParent.getHChildren().add(this);
+        }
+
+        this.sVisited = false;
+    }
+
+    public String getSName() {
+        return sName;
+    }
+
+    public void setSName(String sName) {
+        this.sName = sName;
+    }
+
+    public int getSId() {
+        return sId;
+    }
+
+    public void setSId(int sId) {
+        this.sId = sId;
+    }
+
+    public SignalNode getHParent() {
+        return hParent;
+    }
+
+    public void setHParent(SignalNode hParent) {
+        this.hParent = hParent;
+    }
+
+    public ArrayList<SignalNode> getHChildren() {
+        return hChildren;
+    }
+
+    public void setHChildren(ArrayList<SignalNode> hChildren) {
+        this.hChildren = hChildren;
+    }
+
+    public SignalNode getSParent() {
+        return sParent;
+    }
+
+    public void setSParent(SignalNode sParent) {
+        this.sParent = sParent;
+    }
+
+    public ArrayList<SignalNode> getSChildren() {
+        return sChildren;
+    }
+
+    public void setSChildren(ArrayList<SignalNode> sChildren) {
+        this.sChildren = sChildren;
+    }
+
+    public ArrayList<SignalNode> getSLateralConnections() {
+        return sLateralConnections;
+    }
+
+    public void setSLateralConnections(ArrayList<SignalNode> sLateralConnections) {
+        this.sLateralConnections = sLateralConnections;
+    }
+
+    public boolean getSVisited() {
+        return sVisited;
+    }
+
+    public void setSVisited(boolean sVisited) {
+        this.sVisited = sVisited;
+    }
+}
