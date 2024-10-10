de886ba40b873452b69c1df157df6e02c14409e1
Florian Kämpchen <florian.kaempchen@smail.th-koeln.de>
2024-10-10 10:58:45 +0200

    Add graphCreator.java

---
+++ src/de/thkoeln/fentwums/netlist/backend/parser/graphCreator.java
@@ -0,0 +1,60 @@
+package de.thkoeln.fentwums.netlist.backend.parser;
+
+import org.eclipse.elk.alg.layered.graph.LNode;
+import org.eclipse.elk.graph.ElkNode;
+
+import java.util.HashMap;
+
+import static org.eclipse.elk.graph.util.ElkGraphUtil.createGraph;
+import static org.eclipse.elk.graph.util.ElkGraphUtil.createNode;
+
+public class graphCreator {
+    private ElkNode root;
+
+    public graphCreator() {
+        root = null;
+    }
+
+    public graphCreator(ElkNode root) {
+        this.root = root;
+    }
+
+    public graphCreator(String rootname) {
+        root = createGraph();
+        root.setIdentifier(rootname);
+    }
+
+    public void createGraphFromNetlist(HashMap<String, Object> module, String modulename) {
+        if (root == null) {
+            root = createGraph();
+            root.setIdentifier(modulename);
+        }
+
+        try {
+            checkModuleCompleteness(module);
+        } catch (Exception e) {
+            throw new RuntimeException("Netlist is not complete", e);
+        }
+
+        HashMap<String, Object> ports = (HashMap<String, Object>) module.get("ports");
+        HashMap<String, Object> cells = (HashMap<String, Object>) module.get("cells");
+        HashMap<String, Object> netnames = (HashMap<String, Object>) module.get("netnames");
+    }
+
+    public void checkModuleCompleteness(HashMap<String, Object> module) {
+        if (module == null) {
+            throw new NullPointerException("module is null");
+        }
+        if (!module.containsKey("ports")) {
+            throw new RuntimeException("Module does not contain ports");
+        }
+
+        if (!module.containsKey("cells")) {
+            throw new RuntimeException("Module does not contain cells");
+        }
+
+        if (!module.containsKey("netnames")) {
+            throw new RuntimeException("Module does not contain netnames");
+        }
+    }
+}
