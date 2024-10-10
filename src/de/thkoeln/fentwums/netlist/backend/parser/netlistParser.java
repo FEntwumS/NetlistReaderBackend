5cc57c5bbb579156a97982c61c5f0fc928bedee0
Florian Kämpchen <florian.kaempchen@smail.th-koeln.de>
2024-10-10 10:03:01 +0200

    Update netlistParser.java

--- src/de/thkoeln/fentwums/netlist/backend/parser/netlistParser.java
+++ src/de/thkoeln/fentwums/netlist/backend/parser/netlistParser.java
@@ -10,23 +10,53 @@ public class netlistParser {
     private File netlistFile;
     private ObjectMapper mapper;
     private TypeReference<HashMap<String, Object>> typeRef;
+    private HashMap<String, Object> readNetlist;
 
     public netlistParser() {
         netlistFile = null;
         mapper = new ObjectMapper();
         typeRef = new TypeReference<HashMap<String, Object>>() {};
+        readNetlist = null;
     }
 
     public netlistParser(File netlistFile) {
         this.netlistFile = netlistFile;
         mapper = new ObjectMapper();
         typeRef = new TypeReference<HashMap<String, Object>>() {};
+        readNetlist = null;
     }
 
     public netlistParser(String netlistFilePath) {
         netlistFile = new File(netlistFilePath);
         mapper = new ObjectMapper();
         typeRef = new TypeReference<HashMap<String, Object>>() {};
+        readNetlist = null;
     }
+
+    public File getNetlistFile() {
+        return netlistFile;
+    }
+
+    public void setNetlistFile(File netlistFile) {
+        this.netlistFile = netlistFile;
+    }
+
+    public ObjectMapper getMapper() {
+        return mapper;
+    }
+
+    public void setMapper(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public HashMap<String, Object> getReadNetlist() {
+        return readNetlist;
+    }
+
+    public void setReadNetlist(HashMap<String, Object> readNetlist) {
+        this.readNetlist = readNetlist;
+    }
+
+
 
 }
