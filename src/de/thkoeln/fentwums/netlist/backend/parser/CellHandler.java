2f46d8b8c9a20b1c06981b8eb5e8cdae4fb9313b
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2024-10-11 15:16:24 +0200

    Add CellHandler.java

---
+++ src/de/thkoeln/fentwums/netlist/backend/parser/CellHandler.java
@@ -0,0 +1,19 @@
+package de.thkoeln.fentwums.netlist.backend.parser;
+
+import de.thkoeln.fentwums.netlist.backend.datatypes.HierarchyTree;
+import de.thkoeln.fentwums.netlist.backend.datatypes.SignalTree;
+import org.eclipse.elk.graph.ElkNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CellHandler {
+    public CellHandler() {}
+
+    public ArrayList<HierarchyTree> createCells(HashMap<String, Object> cells, String modulename, ElkNode toplevel,
+                                                ArrayList<SignalTree> signalTreeList) {
+        ArrayList<HierarchyTree> hierarchyTreeList;
+
+        return hierarchyTreeList;
+    }
+}
