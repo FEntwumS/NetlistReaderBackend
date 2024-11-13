4cf64b6b711ecf4eee88a7f21d3c4fe754b86c12
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2024-11-13 10:40:28 +0100

    Create draft set of custom options

--- src/de/thkoeln/fentwums/netlist/backend/options/FEntwumSOptions.java
+++ src/de/thkoeln/fentwums/netlist/backend/options/FEntwumSOptions.java
@@ -13,13 +13,118 @@ public class FEntwumSOptions implements ILayoutMetaDataProvider {
             "de.thkoeln.fentwums.netlist.backend.signalname",
             "");
 
+    public final static IProperty<Integer> SIGNAL_INDEX = new Property<Integer>(
+            "de.thkoeln.fentwums.netlist.backend.signalindex",
+            0
+    );
+
+    public final static IProperty<String> VECTOR_SIGNALS = new Property<String>(
+            "de.thkoeln.fentwums.netlist.backend.vector-signals",
+            ""
+    );
+
+    public final static IProperty<String> CELL_NAME = new Property<String>(
+            "de.thkoeln.fentwums.netlist.backend.cellname",
+            ""
+    );
+
+    public final static IProperty<String> CELL_TYPE = new Property<String>(
+            "de.thkoeln.fentwums.netlist.backend.celltype",
+            ""
+    );
+
+    public final static IProperty<String> LOCATION_PATH = new Property<String>(
+            "de.thkoeln.fentwums.netlist.backend.location-path",
+            ""
+    );
+
+    public static final IProperty<String> SRC_LOCATION = new Property<String>(
+            "de.thkoeln.fentwums.netlist.backend.src-location",
+            ""
+    );
+
+    public static final IProperty<String> SIGNAL_VALUE = new Property<String >(
+            "de.thkoeln.fentwums.netlist.backend.signalvalue",
+            ""
+    );
+
     @Override
     public void apply(Registry registry) {
         registry.register(new LayoutOptionData.Builder()
                 .id("de.thkoeln.fentwums.netlist.backend.signalname")
                 .defaultValue("")
-                .type(LayoutOptionData.Type.BOOLEAN)
-                .optionClass(Boolean.class)
+                .type(LayoutOptionData.Type.STRING)
+                .optionClass(String.class)
+                .targets(EnumSet.of(LayoutOptionData.Target.EDGES))
+                .visibility(LayoutOptionData.Visibility.VISIBLE)
+                .create()
+        );
+
+        registry.register(new LayoutOptionData.Builder()
+                .id("de.thkoeln.fentwums.netlist.backend.signalindex")
+                .defaultValue(0)
+                .type(LayoutOptionData.Type.INT)
+                .optionClass(Integer.class)
+                .targets(EnumSet.of(LayoutOptionData.Target.EDGES))
+                .visibility(LayoutOptionData.Visibility.VISIBLE)
+                .create()
+        );
+
+        registry.register(new LayoutOptionData.Builder()
+                .id("de.thkoeln.fentwums.netlist.backend.vector-signals")
+                .defaultValue("")
+                .type(LayoutOptionData.Type.STRING)
+                .optionClass(String.class)
+                .targets(EnumSet.of(LayoutOptionData.Target.EDGES))
+                .visibility(LayoutOptionData.Visibility.VISIBLE)
+                .create()
+        );
+
+        registry.register(new LayoutOptionData.Builder()
+                .id("de.thkoeln.fentwums.netlist.backend.cellname")
+                .defaultValue("")
+                .type(LayoutOptionData.Type.STRING)
+                .optionClass(String.class)
+                .targets(EnumSet.of(LayoutOptionData.Target.NODES))
+                .visibility(LayoutOptionData.Visibility.VISIBLE)
+                .create()
+        );
+
+        registry.register(new LayoutOptionData.Builder()
+                .id("de.thkoeln.fentwums.netlist.backend.celltype")
+                .defaultValue("")
+                .type(LayoutOptionData.Type.STRING)
+                .optionClass(String.class)
+                .targets(EnumSet.of(LayoutOptionData.Target.NODES))
+                .visibility(LayoutOptionData.Visibility.VISIBLE)
+                .create()
+        );
+
+        registry.register(new LayoutOptionData.Builder()
+                .id("de.thkoeln.fentwums.netlist.backend.location-path")
+                .defaultValue("")
+                .type(LayoutOptionData.Type.STRING)
+                .optionClass(String.class)
+                .targets(EnumSet.of(LayoutOptionData.Target.NODES, LayoutOptionData.Target.EDGES))
+                .visibility(LayoutOptionData.Visibility.VISIBLE)
+                .create()
+        );
+
+        registry.register(new LayoutOptionData.Builder()
+                .id("de.thkoeln.fentwums.netlist.backend.src-location")
+                .defaultValue("")
+                .type(LayoutOptionData.Type.STRING)
+                .optionClass(String.class)
+                .targets(EnumSet.of(LayoutOptionData.Target.NODES, LayoutOptionData.Target.EDGES))
+                .visibility(LayoutOptionData.Visibility.VISIBLE)
+                .create()
+        );
+
+        registry.register(new LayoutOptionData.Builder()
+                .id("de.thkoeln.fentwums.netlist.backend.signalvalue")
+                .defaultValue("")
+                .type(LayoutOptionData.Type.STRING)
+                .optionClass(String.class)
                 .targets(EnumSet.of(LayoutOptionData.Target.EDGES))
                 .visibility(LayoutOptionData.Visibility.VISIBLE)
                 .create()
