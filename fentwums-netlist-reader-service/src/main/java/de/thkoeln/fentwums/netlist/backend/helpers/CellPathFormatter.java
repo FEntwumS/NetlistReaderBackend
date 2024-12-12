fba4b4a75daf0c93a5c0fef7b30d54daed19a150
Florian <florian.kaempchen@smail.th-koeln.de>
2024-12-12 17:08:27 +0100

    docs

--- fentwums-netlist-reader-service/src/main/java/de/thkoeln/fentwums/netlist/backend/helpers/CellPathFormatter.java
+++ fentwums-netlist-reader-service/src/main/java/de/thkoeln/fentwums/netlist/backend/helpers/CellPathFormatter.java
@@ -1,13 +1,24 @@
 package de.thkoeln.fentwums.netlist.backend.helpers;
 
+/**
+ * Provides unified formatting and cleaning of locations of netlist elements. Since the passes that are applied
+ * during netlist generation affect these paths and add potentially confusing patterns, a standardised formatter is
+ * necessary
+ */
 public class CellPathFormatter {
-	public CellPathFormatter() {}
+	public CellPathFormatter() {
+	}
 
+	/**
+	 * Formats and cleans the input string
+	 * @param input The string that is to be formatted and cleaned
+	 * @return The formatted and cleaned string
+	 */
 	public String format(String input) {
 		String output;
 
 		input = input.replaceAll("\\.\\$auto\\$ghdl\\.cc", " ghdl")
-				.replaceAll("\\$auto\\$ghdl\\.cc","ghdl")
+				.replaceAll("\\$auto\\$ghdl\\.cc", "ghdl")
 				.replaceAll("\\$auto\\$opt_dff\\.cc", "opt_dff")
 				.replaceAll("\\$auto\\$ff\\.cc", "auto_ff")
 				.replaceAll("\\$auto\\$opt_reduce\\.cc", "opt_reduce");
--- fentwums-netlist-reader-service/src/main/java/de/thkoeln/fentwums/netlist/backend/helpers/OutputReverser.java
+++ fentwums-netlist-reader-service/src/main/java/de/thkoeln/fentwums/netlist/backend/helpers/OutputReverser.java
@@ -11,9 +11,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Utility class that reverses the output ports to improve the displayed layout by significantly reducing edge crossings
+ */
 public class OutputReverser {
 	public OutputReverser() {}
 
+	/**
+	 * Reverses the output ports of <code>node</code> and assigns all of <code>node</code>'s ports the <code>PORT_INDEX</code> property
+	 * @param node The node whose outputs are to be reversed
+	 */
 	public void reversePorts(ElkNode node) {
 		int index = node.getPorts().size() - 1;
 
@@ -21,6 +28,7 @@ public class OutputReverser {
 			if (port.getProperty(CoreOptions.PORT_SIDE) == PortSide.EAST) {
 				port.setProperty(CoreOptions.PORT_INDEX, index);
 			} else {
+				// count the other way to preserve the order
 				port.setProperty(CoreOptions.PORT_INDEX, node.getPorts().size() + (node.getPorts().size() - index));
 			}
 			index--;
