94d3aeb4ccea049cd879788b0162f50aeb25a38c
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2025-04-14 16:42:16 +0200

    Add PortMarker that marks unconnected ports

---
+++ fentwums-netlist-reader-service/src/main/java/de/thkoeln/fentwums/netlist/backend/helpers/PortMarker.java
@@ -0,0 +1,27 @@
+package de.thkoeln.fentwums.netlist.backend.helpers;
+
+import de.thkoeln.fentwums.netlist.backend.options.FEntwumSOptions;
+import org.eclipse.elk.graph.ElkNode;
+import org.eclipse.elk.graph.ElkPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PortMarker {
+	private static Logger logger = LoggerFactory.getLogger(PortMarker.class);
+
+	public void mark(ElkNode graph) {
+		for (ElkPort port : graph.getPorts()) {
+			markPort(port);
+		}
+
+		for (ElkNode node : graph.getChildren()) {
+			mark(node);
+		}
+	}
+
+	private void markPort(ElkPort port) {
+		if (port.getOutgoingEdges().isEmpty() && port.getIncomingEdges().isEmpty()) {
+			port.setProperty(FEntwumSOptions.NOT_CONNECTED, true);
+		}
+	}
+}
