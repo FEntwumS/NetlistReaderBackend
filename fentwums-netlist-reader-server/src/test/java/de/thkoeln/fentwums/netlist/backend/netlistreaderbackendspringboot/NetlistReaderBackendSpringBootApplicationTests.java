f73aa14a8cf55de30318acff9a7d94d2ffd8f779
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2026-02-25 12:17:03 +0100

    Update test case to use MockMvcTester for MultiPartFile support

--- fentwums-netlist-reader-server/src/test/java/de/thkoeln/fentwums/netlist/backend/netlistreaderbackendspringboot/NetlistReaderBackendSpringBootApplicationTests.java
+++ fentwums-netlist-reader-server/src/test/java/de/thkoeln/fentwums/netlist/backend/netlistreaderbackendspringboot/NetlistReaderBackendSpringBootApplicationTests.java
@@ -1,23 +1,32 @@
 package de.thkoeln.fentwums.netlist.backend.netlistreaderbackendspringboot;
 
+import de.thkoeln.fentwums.netlist.backend.elkoptions.FEntwumSOptions;
+import org.eclipse.elk.alg.layered.options.LayeredOptions;
+import org.eclipse.elk.core.data.LayoutMetaDataService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.assertj.MvcTestResult;
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@SpringBootTest
+@WebMvcTest(NetlistReaderBackendSpringBootApplication.class)
 class NetlistReaderBackendSpringBootApplicationTests {
 
 	@Test
@@ -44,16 +53,38 @@ class NetlistReaderBackendSpringBootApplicationTests {
 
 	@ParameterizedTest
 	@MethodSource("provideTestCasesForAddition")
-	void testAddition(File testcaseInput, File expectedOutput) {
+	void testAddition(File testcaseInput, File expectedOutput, @Autowired MockMvcTester mockMvcTester) {
 		ObjectMapper mapper = new ObjectMapper();
 		TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {
 		};
 
-		// TODO: Get actual JSON from ELK graph
-		HashMap<String, Object> actualMap = mapper.readValue(expectedOutput, typeRef);
+		try (BufferedReader reader = new BufferedReader(new FileReader(testcaseInput))) {
+			StringBuilder builder = new StringBuilder();
 
-		ElkAsserter.assertEquals(expectedOutput, actualMap);
-		//assertEquals(input.getName(), output.getName());
+			String line;
+
+			while ((line = reader.readLine()) != null) {
+				builder.append(line);
+				builder.append("\n");
+			}
+
+			String requestJsonString = builder.toString();
+
+
+			MvcTestResult res = mockMvcTester.post()
+					.uri("/graphRemoteFile?hash=1&performance-target=IntelligentAheadOfTime")
+					.contentType(MediaType.APPLICATION_JSON)
+					.multipart()
+					.file("file", requestJsonString.getBytes(StandardCharsets.UTF_8))
+					.exchange();
+
+			HashMap<String, Object> actualMap = mapper.readValue(res.getResponse().getContentAsString(), typeRef);
+
+			ElkAsserter.assertEquals(expectedOutput, actualMap);
+
+		} catch (Exception e) {
+			// do nothing
+		}
 	}
 
 }
