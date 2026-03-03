5e8572bdd12cba5b299f7e2cea84f0d35dccab24
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2026-03-03 16:49:54 +0100

    Implement Comparable<T> for BundleRange

--- fentwums-netlist-reader-datatypes/src/main/java/de/thkoeln/fentwums/netlist/backend/datatypes/BundleRange.java
+++ fentwums-netlist-reader-datatypes/src/main/java/de/thkoeln/fentwums/netlist/backend/datatypes/BundleRange.java
@@ -2,5 +2,10 @@ package de.thkoeln.fentwums.netlist.backend.datatypes;
 
 import java.util.List;
 
-public record BundleRange(Range containedRange, List<Object> actualDrivers, List<Object> internalDrivers) {
+public record BundleRange(Range containedRange, List<Object> actualDrivers, List<Object> internalDrivers) implements Comparable<BundleRange> {
+	
+	@Override
+	public int compareTo(BundleRange o) {
+		return Integer.compare(this.containedRange().upper(), o.containedRange.upper());
+	}
 }
